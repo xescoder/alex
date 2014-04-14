24e0f7167e08956662a76356d2b195d027aeedcd
xescoder <xescoder@gmail.com>
2014-04-14 18:12:42 +0400

    edit AlexMutation

--- classes/AlexMutation.php
+++ classes/AlexMutation.php
@@ -7,6 +7,8 @@ class AlexMutation
 {
 	private static $alexMutation;
 
+	private $operators;
+
 	/**
 	 * @return AlexMutation
 	 */
@@ -14,7 +16,7 @@ class AlexMutation
 	{
 		$class = get_called_class();
 
-		if (!(self::$alexMutation instanceof $class)) {
+		if (! (self::$alexMutation instanceof $class)) {
 			self::$alexMutation = new $class();
 		}
 
@@ -23,6 +25,84 @@ class AlexMutation
 
 	private function __construct()
 	{
+		$this->operators = $this->readFileToArray(__DIR__ . '/../templates/mutation/operators.tpl');
+	}
+
+	/**
+	 * @param string $path
+	 * @return string[]
+	 */
+	private function readFileToArray($path)
+	{
+		$array = [];
+
+		$file = fopen($path, 'r');
+		while (! feof($file)) {
+			$array[] = fgets($file, 1024);
+		}
+		fclose($file);
+
+		return $array;
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getOperator()
+	{
+		$index = rand(0, count($this->operators) - 1);
+		return $this->operators[$index];
+	}
+
+	private function createVar()
+	{
+		static $tokens = 'qwertyuioplkjhgfdsazxcvbnm';
+		$len = strlen($tokens) - 1;
+
+		$var = '$' . substr($tokens, rand(0, $len), 1);
+
+		if (rand(0, 1000) < 300) {
+			$var .= substr($tokens, rand(0, $len), 1);
+		}
+
+		if (rand(0, 1000) < 200) {
+			$var .= substr($tokens, rand(0, $len), 1);
+		}
+
+		return $var;
+	}
+
+	private function getVars($source)
+	{
+		$vars = [];
+
+		if ($source && preg_match_all('/\$[a-b][a-b0-9_]*/i', $source, $vars)) {
+			if ($vars[0]) {
+				$vars = array_unique($vars[0]);
+			}
+		}
+
+		$len   = count($vars);
+		$start = - ($len / 2);
+		$end   = $len + ($len / 2) + 1;
+		$count = ($len < 3) ? 3 : rand(0, 2);
+
+		if (($len < 3) || (rand(0, 1000) < 300)) {
+			for ($i = 0; $i < $count; $i ++) {
+				$index = rand($start, $end);
+
+				while (TRUE) {
+					$var = $this->createVar();
+					if (! in_array($var, $vars)) {
+						$vars[$index] = $var;
+						break;
+					}
+				}
+			}
+		}
+
+		$vars = array_merge($vars, []);
+		return $vars;
 	}
 
 	/**
@@ -32,6 +112,40 @@ class AlexMutation
 	 */
 	public function mutate($source)
 	{
+		$source = trim($source);
+
+		$vars  = $this->getVars($source);
+		$lines = explode(PHP_EOL, $source);
+		$len   = count($lines);
+
+		$count = rand(0, ($len / 10) + 3);
+		$start = - ($len / 2);
+		$end   = $len + ($len / 2) + 1;
+
+		for ($i = 0; $i < $count; $i ++) {
+			$operator = $this->getOperator();
+			$index    = rand($start, $end);
+
+			$lines[$index] = preg_replace_callback('/\{\$p[0-9]\}/', function () use ($vars) {
+				$index = rand(0, count($vars) - 1);
+				return $vars[$index];
+			}, $operator);
+		}
+
+		foreach ($lines as $index => $line) {
+			$line = trim($line);
+
+			if ($line) {
+				$lines[$index] = $line;
+			}
+			else {
+				unset($lines[$index]);
+			}
+		}
+
+		ksort($lines);
+		$source = implode(PHP_EOL, $lines);
+
 		return $source;
 	}
 } 
